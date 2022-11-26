05e9f67a7f941798238145cf9d980c5f8e775a4a
Dmitriy Chebotkov <dmiitriy4455@gmail.com>
2022-11-26 15:37:49 +0000

    variables and types

--- js/variables_and_types.js
+++ js/variables_and_types.js
@@ -2,7 +2,49 @@
 
 const age = 10;
 const totalPrice = 200.74;
-const userName = 'Chelsey';
-const message = 'Welcome';
+const userName =
+  'Chelsey';
+const message =
+  'Welcome';
 const isOpen = true;
 const shoultConfirm = false;
+
+const type =
+  typeof isOpen; //boolean
+
+console.log(
+  age
+);
+
+console.log(
+  'Before'
+);
+alert(
+  'Price'
+);
+console.log(
+  'After'
+);
+
+const codeReview =
+  confirm(
+    'Subscribe'
+  );
+console.log(
+  codeReview
+);
+
+let quantity =
+  prompt(
+    'Total'
+  );
+quantity =
+  Number(
+    quantity
+  );
+console.log(
+  quantity
+);
+console.log(
+  typeof quantity
+);
