aa598f4f12e295748a4faadfa6d201e0d3218127
Dmitriy Chebotkov <dmiitriy4455@gmail.com>
2022-12-10 17:25:32 +0000

    strings

---
+++ js/strings.js
@@ -0,0 +1,10 @@
+const firstName = "Chelsy";
+const lastName = "Emerald";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+const room = 1;
+const type = "VIP";
+// const welcomeMsg = "Guest" + " " + fullName + " stay in " + type + " number " + room;
+const welcomeMsg = `Guest ${fullName} stay in ${type} number ${room}`;
+console.log(welcomeMsg);
