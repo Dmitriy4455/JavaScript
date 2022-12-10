da18ca6cc17a399e6521f9dc259e212b494f1367
Dmitriy Chebotkov <dmiitriy4455@gmail.com>
2022-12-10 15:51:50 +0000

    numbers

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # javascript
+<!-- {
+  "printWidth": 80,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true,
+  "trailingComa": "all",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "arrowParens": "avoid",
+  "proseWrap": "always"
+} -->
---
+++ js/numbers.js
@@ -0,0 +1,40 @@
+// let elementWidth = "50px";
+// elementWidth = Number.parseInt(elementWidth);
+// console.log("elementWidth:", elementWidth);
+
+// let elementHeigth = "200.74px";
+// elementHeigth = Number.parseFloat(elementHeigth);
+// console.log("elementWidth:", elementHeigth);
+
+// let salary = 1300.12345;
+// salary = Number(salary.toFixed(1));
+// console.log(salary);
+// console.log(Number(salary.toFixed(1)));
+
+// Calculate power
+
+// let base = prompt("Gen Number");
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt("Get Power");
+// power = Number(power);
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result);
+
+// Generate random number
+// --Math.random
+// --Math.round
+// const max = 2;
+// const min = 30;
+// const result = Math.round(Math.random() * (max - min) + min);
+// Example
+// const colors = [`tomato`, `teal`, `orange`, `deeppink`, `skyblue`];
+// const max = colors.length - 1;
+// const min = 0;
+// const index = Math.round(Math.random() * (max - min) + min);
+// const color = colors[index];
+// console.log(color);
+// document.body.style.backgroundColor = color;
