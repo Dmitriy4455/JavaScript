0b77621774fd441f034e93b8f81b8bdc3e14266f
Dmitriy Chebotkov <dmiitriy4455@gmail.com>
2022-12-11 22:17:33 +0000

    operators

--- js/strings.js
+++ js/strings.js
@@ -1,10 +1,17 @@
-const firstName = "Chelsy";
-const lastName = "Emerald";
-const fullName = firstName + " " + lastName;
-console.log(fullName);
+// const firstName = "Chelsy";
+// const lastName = "Emerald";
+// const fullName = firstName + " " + lastName;
+// console.log(fullName);
 
-const room = 1;
-const type = "VIP";
-// const welcomeMsg = "Guest" + " " + fullName + " stay in " + type + " number " + room;
-const welcomeMsg = `Guest ${fullName} stay in ${type} number ${room}`;
-console.log(welcomeMsg);
+// const room = 1;
+// const type = "VIP";
+// // const welcomeMsg = "Guest" + " " + fullName + " stay in " + type + " number " + room;
+// const welcomeMsg = `Guest ${fullName} stay in ${type} number ${room}`;
+// console.log(welcomeMsg);
+
+// let brand = "SamSUnG";
+// brand = brand.toLowerCase();
+// console.log(brand);
+
+// Comparison operators
+console.log((5 && 7 && null && 8) || 5);
