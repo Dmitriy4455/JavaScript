e45233ac81e95a18ea78885e4bef493abcc6ad43
Dmitriy Chebotkov <dmiitriy4455@gmail.com>
2022-12-12 15:54:29 +0000

    loops

--- js/strings.js
+++ js/strings.js
@@ -14,4 +14,60 @@
 // console.log(brand);
 
 // Comparison operators
-console.log((5 && 7 && null && 8) || 5);
+
+// Switch
+const stars = 8;
+let price;
+// if (stars === 1) {
+// 	price = 20;
+// } else if (stars === 2); {
+//     price = 30;
+// } else if (stars === 3); {
+//     price = 50;
+// } else if (stars === 4); {
+//     price = 70;
+// } else if (stars === 5); {
+//     price =120;
+// } else { console. log('Unknown stars rating');
+// }
+
+// switch (stars) {
+// 	case 1:
+// 		price = 20;
+// 		break;
+
+// 	case 2:
+// 		price = 30;
+// 		break;
+
+// 	case 3:
+// 		price = 50;
+// 		break;
+
+// 	case 4:
+// 		price = 70;
+// 		break;
+
+// 	case 5:
+// 		price = 120;
+// 		break;
+
+// 	default:
+// 		console.log("Unknown stars rating");
+// }
+// console.log(price);
+
+// Task
+// Calculate all even numbers
+// const min = 2;
+// const max = 20;
+// let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+// 	console.log(i);
+// 	if (i % 2 === 0) {
+// 		console.log("even", i);
+// 		total += i;
+// 	}
+// }
+// console.log("Total", total);
