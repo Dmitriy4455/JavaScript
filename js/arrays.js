76b30039d2469546a00e2f96f7231c50c0c35b69
Dmitriy Chebotkov <dmiitriy4455@gmail.com>
2022-12-13 13:47:30 +0000

    arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,9 +1,22 @@
-const cart = [34, 35, 23, 34, 22, 33];
-let total = 0;
-// for (let i = 0; i < cart.length; i += 1);
-// total += cart[i];
+// const cart = [34, 35, 23, 34, 22, 33];
+// let total = 0;
+// // for (let i = 0; i < cart.length; i += 1);
+// // total += cart[i];
 
-for (const value of cart) {
-	total += value;
+// for (const value of cart) {
+// 	total += value;
+// }
+// console.log("Total", total);
+
+const logins = ["mango", "poly", "kiwi", "ajax"];
+const loginToFind = "poly";
+let message = " ";
+for (let i = 0; i < logins.length; i += 1) {
+	const login = logins[i];
+	if (login === loginToFind) {
+		message = "User ${loginToFind} is found.";
+		break;
+	}
+	message = "User ${loginToFind} is not found.";
 }
-console.log("Total", total);
+console.log(message);
