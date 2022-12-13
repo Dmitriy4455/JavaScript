a7071c647d64d21073815369462663e40d7d9d3e
Dmitriy Chebotkov <dmiitriy4455@gmail.com>
2022-12-13 17:30:35 +0000

    Update arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -1,3 +1,4 @@
+// 1. Find the total amount
 // const cart = [34, 35, 23, 34, 22, 33];
 // let total = 0;
 // // for (let i = 0; i < cart.length; i += 1);
@@ -8,15 +9,27 @@
 // }
 // console.log("Total", total);
 
-const logins = ["mango", "poly", "kiwi", "ajax"];
-const loginToFind = "poly";
-let message = " ";
-for (let i = 0; i < logins.length; i += 1) {
-	const login = logins[i];
-	if (login === loginToFind) {
-		message = "User ${loginToFind} is found.";
-		break;
-	}
-	message = "User ${loginToFind} is not found.";
-}
-console.log(message);
+// 2. Check the correct username
+// const logins = ["mango", "poly", "kiwi", "ajax"];
+// const loginToFind = "poly";
+// // let message = " ";
+// // for (let i = 0; i < logins.length; i += 1) {
+// // 	const login = logins[i];
+// // 	if (login === loginToFind) {
+// // 		message = "User ${loginToFind} is found.";
+// // 		break;
+// // 	}
+// // 	message = "User ${loginToFind} is not found.";
+// // }
+// const message = logins.includes(loginToFind) ? "Yes" : "No";
+// console.log(message);
+
+//3. Find the smallest number
+// const numbers = [34, 5, 24, 6, 34, 47, 34, 9, 54];
+// let smallestNumber = numbers[0];
+// for (const number of numbers) {
+// 	if (number < smallestNumber) {
+// 		smallestNumber = number;
+// 	}
+// }
+// console.log("Smallest Number", smallestNumber);
